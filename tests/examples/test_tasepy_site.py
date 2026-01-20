32e8a8bbc05b48af36aa54d86f125500845015d9
Dmitry Messerman <dmitry.messerman@gmail.com>
2026-01-20 12:21:24 +0200

    Changed test, as previous one stopped working

--- tests/examples/test_tasepy_site.py
+++ tests/examples/test_tasepy_site.py
@@ -3,5 +3,5 @@ from tasepy.examples import site_checker
 
 def test_tase_api_site(monkeypatch):
     with monkeypatch.context() as m:
-        m.setattr(sys, 'argv', ['tase_api_site_checker','-url','https://datawise.tase.co.il/v1'])
+        m.setattr(sys, 'argv', ['tase_site_checker','-url','https://market.tase.co.il'])
         assert site_checker.main() == 200
